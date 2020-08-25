2d5f924f650e247e390d26367ca209f1447cadc4
Connor Michalec <connormichalec@gmail.com>
2020-08-25 12:51:16 -0500

    Tweaks to mathUtils

--- FluidDynamicsSim2D/MathUtils.hpp
+++ FluidDynamicsSim2D/MathUtils.hpp
@@ -1,4 +1,4 @@
-
+#pragma once
 
 #include <vector>
 #include <memory>
@@ -8,7 +8,7 @@ using namespace std;
 
 /* ------------- 2D VECTOR/MATRIX NORMALIZATION ------------ */
 //Default is float in int out
-vector<vector<unique_ptr<int>>> normalize2DMatrix(vector<vector<unique_ptr<float>>>& in, float inMin, float inMax, float outMin, float outMax) {    //Pass simulationdata as reference, as the only way otherwise would be to move the data in which would erase it from original memory
+static vector<vector<unique_ptr<int>>> normalize2DMatrix(vector<vector<unique_ptr<float>>>& in, float inMin, float inMax, int outMin, int outMax) {    //Pass simulationgrid as reference, as the only way otherwise would be to move the data in which would erase it from original memory
     vector<vector<unique_ptr<int>>> out = vector<vector<unique_ptr<int>>>();
 
 
@@ -23,7 +23,7 @@ vector<vector<unique_ptr<int>>> normalize2DMatrix(vector<vector<unique_ptr<float
             * As I came up with this formula myself, in the future replace with a more
             * efficient one.
             */
-            unique_ptr<int> dim1 = make_unique<int>(round((oldVal*(outMax-outMin))/(inMax-inMin)));
+            unique_ptr<int> dim1 = make_unique<int>(round((oldVal*((float)outMax-(float)outMin))/(inMax-inMin)));
 
             dim2.push_back(std::move(dim1));
         }                                                                                       
@@ -36,5 +36,32 @@ vector<vector<unique_ptr<int>>> normalize2DMatrix(vector<vector<unique_ptr<float
 }
 
 //Overloaded for float out
+static vector<vector<unique_ptr<float>>> normalize2DMatrix(vector<vector<unique_ptr<float>>>& in, float inMin, float inMax, float outMin, float outMax) {    
+    vector<vector<unique_ptr<float>>> out = vector<vector<unique_ptr<float>>>();
+
+
+    /*iterate through the vector and change each value accordingly*/
+    for(int dim2i = 0; dim2i<in.size(); dim2i++) {
+        vector<unique_ptr<float>> dim2 = vector<unique_ptr<float>>();
+
+        for(int dim1i = 0; dim1i<in.at(dim2i).size(); dim1i++) {
+            float oldVal = *in.at(dim2i).at(dim1i);
+
+            /*NOTE: This is probably not a very efficient way of doing this,
+            * As I came up with this formula myself, in the future replace with a more
+            * efficient one.
+            */
+            unique_ptr<float> dim1 = make_unique<float>((oldVal*(outMax-outMin))/(inMax-inMin));
+
+            dim2.push_back(std::move(dim1));
+        }                                                                                       
+
+        out.push_back(std::move(dim2));
+    }
+    /*              */
+
+    return(out);
+}
 
 /* -----------------                        ---------------------- */
+
